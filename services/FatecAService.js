f327b32507b5f2778d539c9c9510a8e29f9dcfd4
wesbarros <weslleywbarros@gmail.com>
2024-03-14 23:38:56 -0300

    feat: Limpando arquivo FatecAService

--- services/FatecAService.js
+++ services/FatecAService.js
@@ -1,23 +1,2 @@
 // Aqui criar o serviço para o banco Mongo
 
-/*Fluxo
-Fazer pull e push pela main
-
-git pull
-git branch - precisa estar na minha
-git checkout weslley -- muda pra minha branch
-git merge main -- vai atualizar minha branch com a main
-------- Fim atualizar minha branch
-
-------- iniciar commit
-git config --list -- conferir se esta remote.origin.url=https://github.com/wesbarros/unibli-backend.git
-        no fim da url precisa apontar para o meu github
-    Caso esteja errado precisa fazer: git remote set url origin <repositorio certo>
-
-    Depois de fazer as alterações na branch certa
-Faz o commit das alterações
-git checkout main -- mudar para a main
-git pull -- para ter certeza que não tem mais arquivo (already up to date)
-git merge weslley -- traz as atualizações que fiz para a main
-git push
-*/
