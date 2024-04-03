06f1dac9faf63e37db032ce7061a019c9c004e64
Helio Okano <helio.okano@fatec.sp.gov.br>
2024-04-03 20:55:04 -0300

    Please enter the commit message for your changes. Lines starting
    Este commit adiciona os modelos necessários para o banco de dados do
    projeto. Foram criados modelos para representar as entidades principais
    do sistema, livro, reserva, fatec, fatec_curso e curso e atualizado o de
    usuário.
    Cada modelo inclui os campos necessários para armazenar as informações
    relevantes, bem como as
    diferentes entidades. Além disso, foram definidas as
    restrições de integridade necessárias para garantir a consistência dos
    dados. Este é um passo importante no desenvolvimento do projeto, pois
    estabelece a estrutura básica do banco de dados.

---
+++ models/Reserva.js
@@ -0,0 +1,20 @@
+// Dá acesso a todos os tipos de dados do banco
+const { DataTypes } = require('sequelize')
+
+// Chama a conexão do banco porque aqui teremos uma operação com o banco, já na definição do model
+const db = require('../db/conn')
+
+//Criação da classe/model
+/** O método define define o módulo 
+ *  define('NomeDoModulo.js', objDeDefinicaoDeTipos{})
+ */
+const Reserva = db.define('Reserva', {
+    dataDaReserva:{
+        //Não precisa definir o attr id, pois ele é criado automaticamente
+        type: DataTypes.DATE,
+        allowNull: false, //NotNull (aceita string vazia)
+    }
+})
+
+
+module.exports = Reserva
